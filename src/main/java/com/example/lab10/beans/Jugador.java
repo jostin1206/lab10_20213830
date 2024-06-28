5dd98f006aa77856c4823b30d658e3fa5abcbc05
Jostin <a20213830@pucp.edu.pe>
2024-06-28 17:00:30 -0500

    bean

---
+++ src/main/java/com/example/lab10/beans/Jugador.java
@@ -0,0 +1,12 @@
+package com.example.lab10.beans;
+
+public class Jugador {
+
+    private int idJugador;
+    private String nombre;
+    private int edad;
+    private String posicion;
+    private String club;
+    private int sn_idSeleccion;
+
+}
